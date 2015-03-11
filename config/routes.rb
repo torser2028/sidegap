713fd6f8208f2c2a7a92f60ceaba6175a65017e4
edooan <gk.edooan@gmail.com>
2015-03-11 14:33:16 -0500

    Create welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   devise_for :users
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
