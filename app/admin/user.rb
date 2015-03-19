59502d741bfcc07918d7e1afb75bb6d71e5d3dde
Edward Parra <eparra@euphoria-net.com>
2015-03-19 18:15:39 -0500

    Custom admins

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,18 +1,13 @@
 ActiveAdmin.register User do
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
+  index do
+    selectable_column
+    column :email
+    column :roles do |user|
+      user.roles.map(&:name).join(' ')
+    end
+    column :created_at
+    actions
+  end
 
 end
