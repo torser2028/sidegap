bc3d825f31361cc0fca1494604a925c2a715cd5a
Edward Parra <eparra@euphoria-net.com>
2015-04-16 11:12:08 -0500

    Comment send email callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,14 +13,14 @@ class User < ActiveRecord::Base
 
   validates :name, :email, :company, :area, :job, presence: true
 
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
 
   def has_role?(role_sym)
     roles.any? { |role| role.name.underscore.to_sym == role_sym }
   end
 
   def send_welcome_email
-    #UserMailer.welcome(self).deliver_now
+    UserMailer.welcome(self).deliver_now
   end
 
 end
