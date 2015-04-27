17fceb5ef0029a7f4f4bfd53058e9dd1c4291655
eparra <eparra@euphoria-net.com>
2015-04-27 14:38:50 -0500

    Rollback again wicked_pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
-if Rails.env.staging? || Rails.env.production?
-  WickedPdf.config = { layout: 'pdf.html', exe_path: '/usr/local/bin/wkhtmltopdf' }
-else
-  WickedPdf.config = { layout: 'pdf.html', exe_path: '/usr/bin/wkhtmltopdf' }
-end
+WickedPdf.config = {
+  #wkhtmltopdf: '/usr/local/bin/wkhtmltopdf',
+  layout: 'pdf.html',
+  exe_path: '/usr/bin/wkhtmltopdf',
+}
