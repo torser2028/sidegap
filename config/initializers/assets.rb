d91e9c6900254fa56440ca70133216ef987f715c
eparra <eparra@euphoria-net.com>
2015-04-27 11:36:35 -0500

    Add assets precompile css and js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( pdf.css )
+Rails.application.config.assets.precompile += %w( blueprint/screen.css jquery.timepicker.css pdf.css jquery.ui.datepicker.js pdf.js jquery.autosize.js jquery.timepicker.js)
