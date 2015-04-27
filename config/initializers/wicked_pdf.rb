fa5a877f9bae8d01251d67f9bf16d9410ea6b946
eparra <eparra@euphoria-net.com>
2015-04-27 15:33:02 -0500

    Trying with no heroku gem and amd64

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
 if Rails.env.staging? || Rails.env.production?
-  WickedPdf.config = { layout: 'pdf.html', exe_path: Rails.root.join('bin', 'wkhtmltopdf').to_s }
+  WickedPdf.config = { layout: 'pdf.html', exe_path: Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s }
 else
   WickedPdf.config = { layout: 'pdf.html', exe_path: '/usr/bin/wkhtmltopdf' }
 end
