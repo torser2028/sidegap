08174d66cea3e745f2a1a7846c00ee0b39bbeabb
eparra <eparra@euphoria-net.com>
2015-04-27 13:16:23 -0500

    Trying with /local

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
-WickedPdf.config = {
-  #wkhtmltopdf: '/usr/local/bin/wkhtmltopdf',
-  layout: 'pdf.html',
-  exe_path: '/usr/bin/wkhtmltopdf',
-}
+if Rails.env.staging? || Rails.env.production?
+  WickedPdf.config = { layout: 'pdf.html', exe_path: '/usr/local/bin/wkhtmltopdf' }
+else
+  WickedPdf.config = { layout: 'pdf.html', exe_path: '/usr/bin/wkhtmltopdf' }
+end
