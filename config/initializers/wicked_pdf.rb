76380cd1dccbcc93237ec7e05fcc18439f7ec05c
eparra <eparra@euphoria-net.com>
2015-04-27 11:17:40 -0500

    Add amd64

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -2,7 +2,7 @@ WickedPdf.config do |variable|
   config.layout = 'pdf.html'
 
   if Rails.env.staging? || Rails.env.production?
-    config.exe_path = Rails.root.join('bin', 'wkhtmltopdf').to_s
+    config.exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
   else
     config.exe_path = '/usr/bin/wkhtmltopdf'
   end
