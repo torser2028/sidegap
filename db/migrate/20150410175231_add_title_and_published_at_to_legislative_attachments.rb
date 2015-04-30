70bce988d6bd010e279ab856bb67a956b20342e8
eparra <eparra@euphoria-net.com>
2015-04-30 18:28:21 -0500

    Delete unnecessary migration

--- db/migrate/20150410175231_add_title_and_published_at_to_legislative_attachments.rb
+++
@@ -1,6 +0,0 @@
-class AddTitleAndPublishedAtToLegislativeAttachments < ActiveRecord::Migration
-  def change
-    add_column :legislative_attachments, :title, :string
-    add_column :legislative_attachments, :published_at, :date
-  end
-end
