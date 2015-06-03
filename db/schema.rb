ea24ab822ab87bf6e1f3f165ef6174fdaf0ed7df
eparra <eparra@euphoria-net.com>
2015-06-03 12:44:10 -0500

    Add warning to legislatives

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528202010) do
+ActiveRecord::Schema.define(version: 20150603162418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -214,9 +214,10 @@ ActiveRecord::Schema.define(version: 20150528202010) do
     t.date     "chamber_commission_at"
     t.date     "chamber_plenary_at"
     t.date     "filing_at"
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.boolean  "active",                default: true
+    t.boolean  "warning",               default: false
   end
 
   create_table "officials", force: :cascade do |t|
