c17aa05c83480ef8c6a2cf0632697e7c90de6c02
eparra <eparra@euphoria-net.com>
2015-06-23 17:04:20 -0500

    Fix scopes

--- app/models/legislative_stakeholder.rb
+++ app/models/legislative_stakeholder.rb
@@ -6,4 +6,6 @@ class LegislativeStakeholder < ActiveRecord::Base
   scope :senate_speakers, -> { where(speaker: true).where(senate: true) }
   scope :chamber_authors, -> { where(author: true).where(chamber: true) }
   scope :chamber_speakers, -> { where(speaker: true).where(chamber: true) }
+  scope :authors, -> { where(author: true) }
+  scope :speakers, -> { where(speaker: true) }
 end
