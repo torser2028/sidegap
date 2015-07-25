d4f23179fd6f79f3b38ee0613fd16e3deeced9a6
Edward Parra <eparra@euphoria-net.com>
2015-07-25 16:15:50 -0500

    Fix wicked_pdf initializer

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,1 @@
-if Rails.env.staging? || Rails.env.production?
-  WickedPdf.config = { layout: 'pdf.html' }
-else
-  WickedPdf.config = { layout: 'pdf.html' }
-end
+WickedPdf.config = { layout: 'pdf.html' }
