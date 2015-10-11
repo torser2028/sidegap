a326a93e3d2940ec842518dbb77caba4be9ea6a3
Edward Parra <gk.edooan@gmail.com>
2015-10-11 15:13:46 -0500

    Fix relationship

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,5 +1,6 @@
 class Institution < ActiveRecord::Base
   belongs_to :sector
+  has_many :rules
   default_scope { where(rule: true).order(name: :asc) }
   scope :executives, -> { where(executive: true) }
   scope :rules, -> { where(rule: true) }
