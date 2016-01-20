7b6debcc61d918090af326f3ba5a65b600353e8b
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2016-01-20 00:26:16 -0500

    Fix server

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',       # required
     :aws_access_key_id      => 'AKIAIWRAWA62U5H66EFQ',       # required
     :aws_secret_access_key  => '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',       # required
-    :region                 => 'us-west-2'  # optional, defaults to 'us-east-1'
+    :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'sidegapfiles' # required
   # see https://github.com/jnicklas/carrierwave#using-amazon-s3
