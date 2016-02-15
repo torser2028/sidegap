cf57dcdce54505d54c72670ff2c66e58f6eb6647
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2016-02-15 02:55:21 -0500

    Implement logo in reports

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def ldate(date, opts = {})
     date ? l(date, opts) : nil
   end
+
+  # require 'open-uri'
+	def embed_remote_image(url, content_type)
+	  asset = open(url, "r:UTF-8") { |f| f.read }
+	  base64 = Base64.encode64(asset.to_s).gsub(/\s+/, "")
+	  "data:#{content_type};base64,#{Rack::Utils.escape(base64)}"
+	end
 end
