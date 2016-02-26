9dc7d2f6975a6bb484b8e3ac37347c478599e244
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2016-02-25 22:04:27 -0500

    Fix issue for images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,12 @@ module ApplicationHelper
 
   # require 'open-uri'
 	def embed_remote_image(url, content_type)
-	  asset = open(url, "r:UTF-8") { |f| f.read }
-	  base64 = Base64.encode64(asset.to_s).gsub(/\s+/, "")
-	  "data:#{content_type};base64,#{Rack::Utils.escape(base64)}"
+		begin
+		  asset = open(url, "r:UTF-8") { |f| f.read }
+		  base64 = Base64.encode64(asset.to_s).gsub(/\s+/, "")
+		  "data:#{content_type};base64,#{Rack::Utils.escape(base64)}"
+		rescue Exception => e
+			nil
+		end
 	end
 end
