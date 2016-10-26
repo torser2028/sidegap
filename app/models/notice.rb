c3ebc36864d4870018e95e7b65990afbe8ff8838
Edward Parra <gk.edooan@gmail.com>
2016-10-26 10:08:01 -0500

    Fix Notice time range

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,4 +1,4 @@
 class Notice < ActiveRecord::Base
-  time_range = ((Time.now - 7.day)..Time.now.midnight + 1.day)
+  time_range = Date.today.beginning_of_week..Date.today.end_of_week
   scope :actual, -> { where(created_at: time_range) }
 end
