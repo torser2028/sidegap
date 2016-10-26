8c550bb609035a4aac381707c62fa5e7ac9562b3
Edward Parra <gk.edooan@gmail.com>
2016-10-25 22:36:20 -0500

    Fix date range

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -2,7 +2,7 @@ class Agenda < ActiveRecord::Base
   belongs_to :legislative
   validates :body, :event_type, :event_at, :time, presence: true
 
-  scope :active, -> { where("event_at >= ?", Date.today) }
-  scope :inactive, -> { where("event_at < ?", Date.today) }
-  scope :past_week, -> { where("event_at > ? AND event_at <= ?", (Date.today - 1.week), Date.today) }
+  scope :active, -> { where("event_at >= ?", Date.today.beginning_of_week) }
+  scope :inactive, -> { where("event_at < ?", Date.today.end_of_week) }
+  scope :past_week, -> { where("event_at > ? AND event_at <= ?", Date.today.last_week, Date.today.last_week.end_of_week) }
 end
