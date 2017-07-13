78cc13a9b18734f00f869b3ed5123db0cbd454d7
EUPHORIANET\jcastaneda <jcastaneda@euphoria-net.com>
2017-07-13 14:29:50 -0500

    Test rules

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -9,7 +9,6 @@ class RulesController < InheritedResources::Base
     @q.result.order(created_at: :desc).each do |item|
       @rules << item unless current_user.following_rules.include? item
     end
-    puts "emma " + @rules
   end
 
   def inactive
