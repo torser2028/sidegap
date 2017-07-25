0763da44d37bd527cd4f482feabe71fdf284b2e7
EUPHORIANET\jcastaneda <jcastaneda@euphoria-net.com>
2017-07-25 15:58:54 -0500

    migrations companies

---
+++ db/migrate/20170725203707_add_emails_to_companies.rb.rb
@@ -0,0 +1,9 @@
+class AddFieldsToCompanies < ActiveRecord::Migration
+  def change
+    add_column :companies, :email_1, :string
+    add_column :companies, :email_2, :string
+    add_column :companies, :email_3, :string
+    add_column :companies, :email_4, :string
+    add_column :companies, :email_5, :string
+  end
+end
