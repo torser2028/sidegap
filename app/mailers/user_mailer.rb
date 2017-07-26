b57239988cb777ae28534d92a212d38441f1ea03
EUPHORIANET\jcastaneda <jcastaneda@euphoria-net.com>
2017-07-26 15:58:05 -0500

    .length > 0 correo empresa

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -22,21 +22,21 @@ class UserMailer < ApplicationMailer
     @name = recipient.name
     mail(to: recipient.email, subject: "Nueva norma en proceso de consulta")
     #en este espacio validar correos de la empresa
-    #puts "julian"
+    puts empresa.email_1
     empresa = Company.find(recipient.company_id)
-    if empresa.email_1
+    if empresa.email_1.length > 0
       mail(to: empresa.email_1, subject: "Nueva norma en proceso de consulta")
     end
-    if empresa.email_2
+    if empresa.email_2.length > 0
       mail(to: empresa.email_2, subject: "Nueva norma en proceso de consulta")
     end
-    if empresa.email_3
+    if empresa.email_3.length > 0
       mail(to: empresa.email_3, subject: "Nueva norma en proceso de consulta")
     end
-    if empresa.email_4
+    if empresa.email_4.length > 0
       mail(to: empresa.email_4, subject: "Nueva norma en proceso de consulta")
     end
-    if empresa.email_5
+    if empresa.email_5.length > 0
       mail(to: empresa.email_5, subject: "Nueva norma en proceso de consulta")
     end
     #puts empresa.name
