28d0016cb5cb03b42ed5e2ff5e44f813da18dbf9
EUPHORIANET\jcastaneda <jcastaneda@euphoria-net.com>
2017-07-27 17:52:58 -0500

    cambio accepts_nested_attributes_for

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -5,8 +5,8 @@ class Rule < ActiveRecord::Base
   has_many :attachments, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  #accepts_nested_attributes_for :attachments, allow_destroy: true, :tags
-  accepts_nested_attributes_for :tags, :attachments, allow_destroy: true
+  accepts_nested_attributes_for :attachments, allow_destroy: true
+  #accepts_nested_attributes_for :tags, :attachments, allow_destroy: true
 
   scope :inactive, -> { where("deadline_comments < ?", Date.today) }
   scope :active, -> { where("deadline_comments >= ? OR deadline_comments IS NULL", Date.today) }
