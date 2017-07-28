4d3fd3441d5ea1c84eb6c43c3fd5c0bf6f0b95dc
EUPHORIANET\jcastaneda <jcastaneda@euphoria-net.com>
2017-07-28 11:51:53 -0500

    Titulo

--- app/admin/rule.rb
+++ app/admin/rule.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register Rule do
     attachments_attributes: [:id, :_destroy, :attachment, :title, :published_at]
 
   csv do
-    column('Titulo', humanize_name: false) {:title}
+    column(:title) {"Titulo"}
     column("Tipo de norma") {:kind}
     column("Institución") {:institution_id}
     column("Fecha de creación") {:filing_at}
