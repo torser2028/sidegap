4ae00a306a9321e10087cb5c73913d993b865480
EUPHORIANET\jcastaneda <jcastaneda@euphoria-net.com>
2017-07-28 09:42:18 -0500

    exclude_fields :id

--- app/admin/rule.rb
+++ app/admin/rule.rb
@@ -4,6 +4,8 @@ ActiveAdmin.register Rule do
   permit_params :title, :kind, :institution_id, :filing_at, :deadline_comments, :for_comments,
     attachments_attributes: [:id, :_destroy, :attachment, :title, :published_at]
 
+  exclude_fields :id
+
   filter :title, label: "Titulo"
   filter :kind, label: "Tipo de Norma", as: :select, collection: -> { Kind.rules.pluck(:name) }
   filter :institution, label: "Institución", as: :select, collection: -> { Institution.rules }
@@ -96,5 +98,6 @@ ActiveAdmin.register Rule do
       @page_title = "Editar Norma"
     end
   end
+
 
 end
