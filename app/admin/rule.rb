dbe687f05a2a12575753563458738b60dff41267
EUPHORIANET\jcastaneda <jcastaneda@euphoria-net.com>
2017-07-28 12:24:59 -0500

    other

--- app/admin/rule.rb
+++ app/admin/rule.rb
@@ -7,12 +7,12 @@ ActiveAdmin.register Rule do
     attachments_attributes: [:id, :_destroy, :attachment, :title, :published_at]
 
   csv do
-    column("Titulo"){|rule| rule.title.encode("iso-8859-1").force_encoding("utf-8")}
-    column("Tipo de norma"){|rule| rule.kind.encode("iso-8859-1").force_encoding("utf-8")}
+    column("Titulo"){|rule| rule.title}
+    column("Tipo de norma"){|rule| rule.kind}
     column("Institución") {|rule| rule.institution_id}
     column("Fecha de creación") {|rule| rule.filing_at}
     column("Fecha limite para comentarios") {|rule| rule.deadline_comments}
-    column("Comentarios") {|rule| rule.for_comments.encode("iso-8859-1").force_encoding("utf-8")}
+    column("Comentarios") {|rule| rule.for_comments}
     column("Ruta") {|rule| "https://app.sidegap.com/admin/rules/" + rule.id.to_s}
   end
 
