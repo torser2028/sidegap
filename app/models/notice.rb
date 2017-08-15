4807b3e226dc0b73eb05ae3a67aee169ec989776
altose87 <altose87@gmail.com>
2017-08-14 23:20:25 -0500

    Change the date range to time range to find the Notices

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -4,6 +4,6 @@ class Notice < ActiveRecord::Base
   
   validates :body, :user, presence: true
 
-  time_range = Date.today.beginning_of_week..Date.today.end_of_week
+  time_range = Date.today.beginning_of_week.beginning_of_day..Date.today.end_of_week.end_of_day
   scope :actual, -> { where(created_at: time_range) }
 end
