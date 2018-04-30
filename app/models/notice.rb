9db18abfc12d0d78fd21447443a576bca6ade5e1
Alejandro Toro <alejandro@kommit.co>
2018-04-30 16:23:34 -0500

    Change parser version and force to start the weekend on monday

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -3,6 +3,6 @@ class Notice < ActiveRecord::Base
 
   validates :body, :user, :publish_date, presence: true
 
-  time_range = Date.today.beginning_of_week.beginning_of_day..Date.today.end_of_week.end_of_day
+  time_range = Date.today.beginning_of_week(start_day = :monday).beginning_of_day..Date.today.end_of_week(start_day = :monday).end_of_day
   scope :actual, -> { where(publish_date: time_range) }
 end
