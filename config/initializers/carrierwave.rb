594d70bf4ce906ef78682856d1be09dbf098afc2
Alejandro Toro <alejandro@kommit.co>
2018-08-08 13:57:46 -0500

    Change variable names

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'AKIAIWRAWA62U5H66EFQ',       # required
-    :aws_secret_access_key  => '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',       # required
+    :aws_access_key_id      => 'AKIAJKLPXNK4BPWRTCTA',       # required
+    :aws_secret_access_key  => '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',       # required
     :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'sidegapfiles' # required
+  config.fog_directory  = 'sidegapfilesv2' # required
   # see https://github.com/jnicklas/carrierwave#using-amazon-s3
   # for more optional configuration
 end
