acd6c36cd36709980bc4dd9f700579580c8ebf91
Alejandro Toro <alejandro@kommit.co>
2019-12-11 11:35:56 -0500

    Change config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,114 +1,145 @@
-# # Change these
-# server '13.77.86.187', port: 22, roles: [:web, :app, :db], primary: true
-
-# set :repo_url,        'git@github.com:torser2028/sidegap.git'
-# set :application,     'SIDEGAP'
-# set :user,            'Administrador'
-# set :puma_threads,    [4, 16]
-# set :puma_workers,    0
-
-# # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/application.yml')
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'vendor/bundle', 'tmp/pids', 'tmp/sockets')
-# append :rbenv_map_bins, 'puma', 'pumactl'
-
-# # Don't change these unless you know what you're doing
-# set :pty,             true
-# set :use_sudo,        false
-# set :stage,           :production
-# set :deploy_via,      :remote_cache
-# set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-# set :puma_access_log, "#{release_path}/log/puma.error.log"
-# set :puma_error_log,  "#{release_path}/log/puma.access.log"
-# set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-# set :puma_preload_app, true
-# set :puma_worker_timeout, nil
-# set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
-# ## Defaults:
-# # set :scm,           :git
-# # set :branch,        :master
-# # set :format,        :pretty
-# # set :log_level,     :debug
-# # set :keep_releases, 5
-
-# ## Linked Files & Directories (Default None):
-# # set :linked_files, %w{config/database.yml}
-# # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :logtail_files, %W( /home/#{fetch(:user)}/apps/#{fetch(:application)}/current/log/#{fetch(:rails_env)}.log )
-# set :logtail_lines, 50
-
-# namespace :puma do
-#   desc 'Create Directories for Puma Pids and Socket'
-#   task :make_dirs do
-#     on roles(:app) do
-#       execute "mkdir #{shared_path}/tmp/sockets -p"
-#       execute "mkdir #{shared_path}/tmp/pids -p"
-#     end
-#   end
-
-#   before :start, :make_dirs
-# end
-
-# namespace :deploy do
-#   desc "Make sure local git is in sync with remote."
-#   task :check_revision do
-#     on roles(:app) do
-#       unless `git rev-parse HEAD` == `git rev-parse origin/master`
-#         puts "WARNING: HEAD is not the same as origin/master"
-#         puts "Run `git push` to sync changes."
-#         exit
-#       end
-#     end
-#   end
-
-#   desc 'Initial Deploy'
-#   task :initial do
-#     on roles(:app) do
-#       before 'deploy:restart', 'puma:start'
-#       invoke 'deploy'
-#     end
-#   end
-
-#   # desc 'Restart application'
-#   # task :restart do
-#   #   on roles(:app), in: :sequence, wait: 5 do
-#   #     invoke!("puma:restart")
-#   #   end
-#   # end
-
-#   before :starting,     :check_revision
-#   after  :finishing,    :compile_assets
-#   after  :finishing,    :cleanup
-#   after  :finishing,    :restart
-# end
-
-# # ps aux | grep puma    # Get puma pid
-# # kill -s SIGUSR2 pid   # Restart puma
-# # kill -s SIGTERM pid   # Stop puma
-
-# require "bundler/capistrano"
-# require "rvm/capistrano"
-
-lock '3.4.0'
+# Change these
 server '13.77.86.187', port: 22, roles: [:web, :app, :db], primary: true
 
-set :user,            'Administrador'
-set :application,     'SIDEGAP'
 set :repo_url,        'git@github.com:torser2028/sidegap.git'
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :log_level, :debug
+set :application,     'SIDEGAP'
+set :user,            'Administrador'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/application.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'vendor/bundle', 'tmp/pids', 'tmp/sockets')
+append :rbenv_map_bins, 'puma', 'pumactl'
+
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
+# set :keep_releases, 5
+
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :logtail_files, %W( /home/#{fetch(:user)}/apps/#{fetch(:application)}/current/log/#{fetch(:rails_env)}.log )
+set :logtail_lines, 50
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
 
 namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    invoke 'unicorn:restart'
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
   end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  # desc 'Restart application'
+  # task :restart do
+  #   on roles(:app), in: :sequence, wait: 5 do
+  #     invoke!("puma:restart")
+  #   end
+  # end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
 end
-after 'deploy:publishing', 'deploy:restart'
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
+
+# # WITH UNICORN
+# # Define your server here
+# server '13.77.86.187', port: 22, roles: [:web, :app, :db], primary: true
+
+# # Set application settings
+# set :application,     'SIDEGAP'
+# set :user,            'Administrador'
+# set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+# set :deploy_via, :remote_cache
+# set :use_sudo, false
+
+# set :scm, "git"
+# set :repository, 'git@github.com:torser2028/sidegap.git'
+# set :branch, "master"
+
+# set :pty, true
+# set :forward_agent, true
+
+# after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+# namespace :deploy do
+#   %w[start stop restart].each do |command|
+#     desc "#{command} unicorn server"
+#     task command, roles: :app, except: {no_release: true} do
+#       run "/etc/init.d/unicorn_#{application} #{command}" # Using unicorn as the app server
+#     end
+#   end
+
+#   task :setup_config, roles: :app do
+#     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+#     sudo "ln -nfs #{current_path}/config/unicorn_ini.sh /etc/init.d/unicorn_#{application}"
+#     run "mkdir -p #{shared_path}/config"
+#     put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
+#     puts "Now edit the config files in #{shared_path}."
+#   end
+#   after "deploy:setup", "deploy:setup_config"
+
+#   task :symlink_config, roles: :app do
+#     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+#   end
+#   after "deploy:finalize_update", "deploy:symlink_config"
+
+#   desc "Make sure local git is in sync with remote."
+#   task :check_revision, roles: :web do
+#     unless `git rev-parse HEAD` == `git rev-parse origin/master`
+#       puts "WARNING: HEAD is not the same as origin/master"
+#       puts "Run `git push` to sync changes."
+#       exit
+#     end
+#   end
+#   before "deploy", "deploy:check_revision"
+# end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,24 @@
+# Define your root directory
 root = "/home/Administrador/apps/SIDEGAP/current"
+
+# Define worker directory for Unicorn
 working_directory root
+
+# Location of PID file
 pid "#{root}/tmp/pids/unicorn.pid"
+
+# Define Log paths
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.SIDEGAP.sock"
-worker_processes 2
-timeout 30
+# Listen on a UNIX data socket
+listen "/tmp/unicorn.gifroll.sock"
 
-# Force the bundler gemfile environment variable to
-# reference the capistrano "current" symlink
-before_exec do |_|
-  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
-end
+# 16 worker processes for production environment
+worker_processes 16
+
+# Load rails before forking workers for better worker spawn time
+preload_app true
+
+# Restart workes hangin' out for more than 240 secs
+timeout 240
