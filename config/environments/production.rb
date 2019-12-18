30b59992ddff74edad45a4afb9a7c858c9d275ec
Alejandro Toro <alejandro@kommit.co>
2019-12-18 17:41:10 -0500

    enable again the email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,18 +85,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'app.sidegap.com' }
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.asset_host = 'http://app.sidegap.com'
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.asset_host = 'http://app.sidegap.com'
 
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.office365.com',
-  #   port: '587',
-  #   authentication: :login,
-  #   user_name: ENV['MAILER_USERNAME'],
-  #   password: ENV['MAILER_PASSWORD'],
-  #   domain: 'app.sidegap.com',
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.office365.com',
+    port: '587',
+    authentication: :login,
+    user_name: ENV['MAILER_USERNAME'],
+    password: ENV['MAILER_PASSWORD'],
+    domain: 'app.sidegap.com',
+    enable_starttls_auto: true
+  }
 end
